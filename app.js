ae9d41c77675e32b6b7510104045c38b100aa1f6
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-18 17:49:22 -0500

    Got the node express starting to work, going to continue backend for a while

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const admin = require('firebase-admin');
-// TODO: delete this when ready to deploy, not required
-const serviceAccount = require('./service-account.json')
+const express = require("express");
+const firebase = require('./firebase');
 
-admin.initializeApp({
-    // for when we deploy to google cloud platform
-    // credential: admin.credential.applicationDefault()
-    credential: admin.credential.cert(serviceAccount)
+const app = express();
+const port = 8080;
 
-});
+app.listen(port, () => {
+    console.log(`Listening at http://localhost:${port}`)
+})
 
-const db = admin.firestore();
-
+app.get("/", (req, res) => {
+    res.send(`<p>${firebase.getGame()}</p>`)
+})
---
+++ firebase.js
@@ -0,0 +1,16 @@
+const admin = require('firebase-admin');
+// TODO: delete this when ready to deploy, not required
+const serviceAccount = require('./service-account.json')
+
+admin.initializeApp({
+    // for when we deploy to google cloud platform
+    // credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert(serviceAccount)
+
+});
+
+const db = admin.firestore();
+
+exports.getGame = (gameId) => {
+    return "This will be a game id";
+}
