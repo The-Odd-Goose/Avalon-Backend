f516830e36786cc9b3e6341d36461a1d19c1cd9d
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-19 21:33:52 -0500

    Working on creating a game, next create a uuid

--- firebase.js
+++ firebase.js
@@ -10,7 +10,34 @@ admin.initializeApp({
 });
 
 const db = admin.firestore();
+const gamesRef = db.collection("games");
 
-exports.getGame = (gameId) => {
-    return "This will be a game id";
+// here we'll define the function that enters the game for us
+exports.enterGame = (userId, gameId) => {
+
+    return {
+        gameId
+    };
+}
+
+// so this will be how the new game is
+// players array will hold all the players, while turn is which player in that array's turn to suggest a new batch of players
+// and voted will be an array of currently proposed ppl to go on the mission with
+const newGame = (userId) => ({
+    fail: 0,
+    players: [userId],
+    rejected: 0,
+    success: 0,
+    turn: 0,
+    voted: []
+})
+
+// here we'll define a function that creates a game for us
+exports.createGame = (userId) => {
+
+    const gameId = 
+
+    await gamesRef.doc().add({
+        newGame()
+    })
 }
