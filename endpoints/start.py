723aad939cdc5998736413d5981751efffd0b03f
Eric Huang <eric.haoran.huang@gmail.com>
2020-11-26 21:23:05 -0500

    got firebase admin sdk installed

--- endpoints/start.py
+++ endpoints/start.py
@@ -1,4 +1,13 @@
 from flask import Blueprint, request
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate('../service-account.json')
+default_app = firebase_admin.initialize_app(cred)
+# TODO: switch to this for deployment
+# default_app = firebase_admin.initialize_app()
+
+db = firestore.client()
 
 # so here we'll define endpoints to start the game
 start = Blueprint('start', __name__)
