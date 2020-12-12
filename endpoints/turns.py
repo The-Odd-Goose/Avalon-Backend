e8124fb2720a38e404e6953ac8d86508e5b198ab
Eric Huang <eric.haoran.huang@gmail.com>
2020-12-11 21:15:50 -0500

    Got the propose mission endpoint to start working a bit

--- endpoints/turns.py
+++ endpoints/turns.py
@@ -1,11 +1,20 @@
+from endpoints.type import GameIDError
 from flask import Blueprint, request, jsonify
-from endpoints.firebase import db, doesGameExist, getGameDict, getGameRef
+from endpoints.firebase import db, doesGameExist, doesUserExistInGame, getGameDict, getGameRef
 
 turns = Blueprint('turns', __name__)
 
 # given a certain turn, and number of players, will return the 
 def pplOnMission(turn: int, ppl: int) -> int:
-    pass
+    # hold the array of mission and people here:
+    missions = [
+        [2, 3, 2, 3, 3], # 5 people
+        [2, 3, 3, 3, 4], # 6 people
+        [3, 4, 3, 4, 5], # 7 people
+        [3, 4, 4, 4, 5] # 8 people
+    ]
+
+    return missions[ppl - 5][turn - 1]
 
 # endpoint to propose missions
 @turns.route("/proposeMission", methods=['POST'])
@@ -18,19 +27,47 @@ def proposeMission():
     #   uid: their user id
     # }
 
-    # TODO: check user id with players.missionmaker's uid -- import from firebase
-    # TODO: add in the dictionaries for the different size of the groups,
-    # TODO: its turns, and then corresponding mission sizes
+    # TODO: test this!
 
     data = request.json
-    game_ref = doesGameExist(data)
+    try: 
+        game_ref = doesGameExist(data)
+        game = game_ref.get().to_dict()
 
-    game_ref.update({
-        u'mission': data.get('mission')
-    })
+        uid = data.get("uid")
+        if uid is None or uid == "":
+            return "error... uid not found... login"
 
-    return "Success! Added ppl to mission!"
 
+        players_ref = game_ref.collection(u'players')
+
+        # have to check whether it is the right player
+        mission_maker = players_ref.document(game.get("missionMaker")).get().to_dict()
+        if uid != mission_maker.get("uid"):
+            return "Not the mission maker cannot do that!"
+
+        players_lst = [p for p in players_ref.stream()] # gets the list of players
+
+        num_mission = pplOnMission(game.get("turn"), len(players_lst))
+
+        mission = data.get('mission')
+
+        if not type(mission) == list:
+            return "Not proper format!"
+
+        if len(mission) < num_mission:
+            return "Not enought people on mission to set!"
+        
+        game_ref.update({
+            u'mission': data.get('mission')
+        })
+
+        return "Success! Added ppl to mission!"
+
+    except GameIDError as e:
+        return e.message
+
+# TODO: figure some more shit out dude
 @turns.route("/voteMission", methods = ['POST'])
 def vote():
 
